f5136e499b45e625ded0bf0bceb5ff5b8aab245d
Chieu Duong <dchieu@bocasay.com>
2020-08-06 14:12:53 +0700

    more update

--- src/core/main.js
+++ src/core/main.js
@@ -1,67 +1,68 @@
-const { app, BrowserWindow } = require('electron')
-const { autoUpdater } = require("electron-updater")
+const { app, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
-let win
+autoUpdater.requestHeaders = {
+  GH_TOKEN: "311c42e68ee580b07d071794f0f5a3475cbafb69",
+};
+// autoUpdater.autoDownload = true;
+
+let win;
 
 const dispatch = (data) => {
-  win.webContents.send('message', data)
-}
+  win.webContents.send("message", data);
+};
 
 const createDefaultWindow = () => {
-  win = new BrowserWindow()
+  win = new BrowserWindow();
 
-  win.on('closed', () => {
-    win = null
-  })
+  win.on("closed", () => {
+    win = null;
+  });
 
-  win.loadFile('src/gui/index.html')
+  win.loadFile("src/gui/index.html");
 
-  return win
-}
+  return win;
+};
 
-app.on('ready', () => {
-  
-  createDefaultWindow()
+app.on("ready", () => {
+  createDefaultWindow();
 
-  autoUpdater.checkForUpdatesAndNotify()
+  autoUpdater.checkForUpdatesAndNotify();
 
-  win.webContents.on('did-finish-load', () => {
-    win.webContents.send('version', app.getVersion())
-  })
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.send("version", app.getVersion());
+  });
+});
 
-})
+app.on("window-all-closed", () => {
+  app.quit();
+});
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
+autoUpdater.on("checking-for-update", () => {
+  dispatch("Checking for update...");
+});
 
+autoUpdater.on("update-available", (info) => {
+  dispatch("Update available.");
+});
 
-autoUpdater.on('checking-for-update', () => {
-  dispatch('Checking for update...')
-})
+autoUpdater.on("update-not-available", (info) => {
+  dispatch("Update not available.");
+});
 
-autoUpdater.on('update-available', (info) => {
-  dispatch('Update available.')
-})
+autoUpdater.on("error", (err) => {
+  dispatch("Error in auto-updater. " + err);
+});
 
-autoUpdater.on('update-not-available', (info) => {
-  dispatch('Update not available.')
-})
-
-autoUpdater.on('error', (err) => {
-  dispatch('Error in auto-updater. ' + err)
-})
-
-autoUpdater.on('download-progress', (progressObj) => {
+autoUpdater.on("download-progress", (progressObj) => {
   // let log_message = "Download speed: " + progressObj.bytesPerSecond
   // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
   // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
   // dispatch(log_message)
 
-    win.webContents.send('download-progress', progressObj.percent)
+  win.webContents.send("download-progress", progressObj.percent);
+});
 
-})
-
-autoUpdater.on('update-downloaded', (info) => {
-  dispatch('Update downloaded')
-})
+autoUpdater.on("update-downloaded", (info) => {
+  dispatch("Update downloaded");
+});
